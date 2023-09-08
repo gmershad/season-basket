e54df1b3f09f5b188a8d04678571eee8621ab71b
Gul Ershad <gul.ershad@namshi.org>
2023-09-09 01:54:34 +0530

    Added security

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const expressApp = require('./infrastructure/middlewares/express-app');
 require('./models');
 const dbConnect = require('./infrastructure/database');
 const compression = require("compression");
+const csrf = require("csurf");
+const cookieParser = require("cookie-parser");
 
 async function createApp() {
     try {
@@ -10,8 +12,12 @@ async function createApp() {
         //await dbConnect.sync({ alter: true });
         await dbConnect.sync();
         const app = express();
+        app.use(cookieParser());
+        const csrfProtection = csrf({ cookie: true });
+        app.use(csrfProtection);
         app.use(compression());
         await expressApp(app);
+        app.use((req, res, next) => { res.locals.csrfToken = req.csrfToken(); next(); });
         return app;
     } catch (error) {
         console.error('Error connecting to the database:', error);
--- src/infrastructure/middlewares/multer-config.js
+++ src/infrastructure/middlewares/multer-config.js
@@ -13,6 +13,9 @@ const storage = multer.diskStorage({
     },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+});
 
 module.exports = upload;
